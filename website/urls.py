606966fc2198534ffc451a0a5706167f4a5dd092
alizamanian79 <alizamaniandev@gmail.com>
2024-10-10 11:47:26 +0330

    basic created

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index_view,newpage_view
+
+app_name="website"
+urlpatterns = [
+    path("",index_view,name="index"),
+    path("new/<str:slug>/",newpage_view,name="new-page"),
+]
