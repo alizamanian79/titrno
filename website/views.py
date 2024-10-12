57d7237a4d27224da2f8dc7f645f8ad698513879
alizamanian79 <alizamaniandev@gmail.com>
2024-10-12 11:03:07 +0330

    404 created

--- website/views.py
+++ website/views.py
@@ -4,23 +4,25 @@ from django.contrib import messages
 from .models import New
 from .forms import ContactForm
 from django.db.models import Q
+from django.http import Http404
+
 
 # Create your views here.
-
 def index_view(request):
+    
+
     return render(request, 'website/index.html')
 
 
 
 
-
-
 def newpage_view(request, slug=None):  
+
+
     searchNews = []  
     q = request.GET.get("q")  
 
     if q:  
-        
         searchNews = New.objects.filter(Q(title__icontains=q) | Q(description__icontains=q) | Q(slug__icontains=q))  
 
     if request.method=="POST":
