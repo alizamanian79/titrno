627bf6497f6b02f0e6f8332e169e2a5742d2c5c4
alizamanian79 <alizamaniandev@gmail.com>
2024-10-19 10:34:47 +0330

    footer template tag created

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,6 +1,6 @@
 from django import template
 register = template.Library()
-from website.models import  New,Category
+from website.models import  New,Category,Tag
 from datetime import datetime 
 
 
@@ -9,8 +9,29 @@ def recent_news():
     News=New.objects.filter(active=True).order_by("-updated_at")[:4]
     return {"News":News}
 
-@register.simple_tag()
-def getCat():
+
+
+
+@register.inclusion_tag("footer.html")
+def footer():
+    allcategories=Category.objects.all()
+    new=New.objects.all()
+    categories_count={}
+    tags_count={}
+    alltags=Tag.objects.all()
+
+    for cat in allcategories:
+        categories_count[cat]=new.filter(categories__title=cat).count()
+    
+    for tag in alltags:
+        tags_count[tag]=new.filter(tags__title=tag).count()
+    
+    return {'categories_count':categories_count,"tags_count":tags_count}
+    
+
+
+# @register.simple_tag()
+# def getCat():
     categories=Category.objects.all()
     new=New.objects.all(active=True)
     categories_count={}
