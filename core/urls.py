4ec3553ca3db59ca0e2c058b01a05587ca4cd669
alizamanian79 <alizamaniandev@gmail.com>
2024-10-25 14:44:41 +0330

    basic

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
-from django.conf import settings
-from django.views.static import serve
 
 urlpatterns = [
     
@@ -13,10 +10,10 @@ urlpatterns = [
     path('', include('website.urls')),
 
 
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
+
+# Serve static files in development  
 if settings.DEBUG:  
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
