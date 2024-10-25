20943a4f888c76ee1d8d76704291b51859a9eca7
alizamanian79 <alizamaniandev@gmail.com>
2024-10-25 12:25:53 +0330

    if debug

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,6 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-
-if settings.DEBUG:  
+if not settings.DEBUG:  
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
