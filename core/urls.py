f06d7d693402a873aec25db4ba9006635c134e23
alizamanian79 <alizamaniandev@gmail.com>
2024-10-25 13:24:29 +0330

    logi ui changes

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-if not settings.DEBUG:  
+if settings.DEBUG:  
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
