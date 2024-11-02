9438b1ce7ce8f51b3158a1278d66a4f4ce07db14
alizamanian79 <alizamaniandev@gmail.com>
2024-11-02 20:43:30 +0330

    test

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Admin site  
     path('accounts/', include('accounts.urls')),  # Include URLs from accounts app  
     path('', include('website.urls')),  # Include URLs from website app  
-
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]  
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
